049afbffb24207a7951b5a22636e71a21940ece2
Adam-Hachem <adam.hachem@mymail.champlain.edu>
2023-02-19 20:09:40 -0500

    secure ssh

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author Adam-Hachem
+#Creates a new ssh user using $1 parameter
+#Adds a public key from the local repo or curled from the remote repo
+#Removes root's ability to ssh in
+echo "All YOUR CODE GOES HERE"
